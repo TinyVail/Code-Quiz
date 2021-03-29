9339050c765c437830ba82cf16a0ca4f17bc01b8
ClancyGilroy <78936857+TinyVail@users.noreply.github.com>
2021-03-28 20:50:31 -0500

    final commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,92 +1,119 @@
 // setInterval(functionToCall, intervalTime)
 
+/*
+
+    let x =5;
+    let y = "hello";
+    let z = [5, 6, 7];
+    let obj = {
+        question: "this is the question",
+        answers: ["answer 1", "answer 2"],
+        correctAnswer: 0
+    };
+*/
+
 $(window).on('load', function () {
 
-	let questions = [
-		"Question 1: blah blah blah?",
-		"Question 2: blah blah blah?",
-		"Question 3: blah blah blah?",
-        "Question 4: blah blah blah?",
-        "Question 5: blah blah blah?"
-	];
+    let questions = [
+        "How do you check if two values are equal in JavaScript?",
+        "Which is not a valid way to define a function in JavaScript?",
+        "What is the difference between var and let in JavaScript?"
+    ];
 
-	let answers = [
-		/*0:*/["answer 1", "answer 2", "answer 3", "answer 4"]
-		/*1:*/["answer 1", "answer 2", "answer 3", "answer 4"]
-        ["answer 1", "answer 2", "answer 3", "answer 4"]
-        ["answer 1", "answer 2", "answer 3", "answer 4"]
-        ["answer 1", "answer 2", "answer 3", "answer 4"]
-	]
-	//answers[0][0] = "answer 1"
-	//answers[1][1] = "question 2 answer 2"
-    // arrays start at 0 so answer 2 is 1 shown above
+    let answers = [
+        [
+            "if(x1 = x2)",
+            "(x1 == x2) if:",
+            "if(x1 equals x2)",
+            "if(x1.equals(x2)",
+            "if(x1 == x2)"
+        ],
+        [
+            "let x = new function(){};",
+            "function x(){}",
+            "const x = () => {}",
+            "let x = function() {};"
+        ],
+        [
+            "'var' creates a variable, 'let' lets code execute",
+            "'let' creates a variable, 'var' updated a variable",
+            "'let' is globally scoped, whereas 'var' is locally scoped",
+            "'var' is globally scoped, whereas 'let' is locally scoped",
+            "They are exactly the same"
+        ]
+    ];
 
-	let currentQuestion = 0;
+    let correctAnswers = [4, 0, 3];
+
+    let userScore = 0;
+
+    let currentQuestion = 0;
 
     let startTime = new Date();
 
+    let testTime = 10000 * 60; // 10 minutes
+
+    let interval;
+
     const timeInput = () => {
-        let secondsPassed = (new Date() - startTime) / 1000;
-		let formattedTime = new Date(secondsPassed * 1000).toISOString().substr(11, 8);
-        $("#timer").html("<p> Time Passed: " + formattedTime + " seconds</p>");
+        let msPassed = new Date() - startTime;
+        let timeRemaining = testTime - msPassed;
+        let formattedTime = new Date(timeRemaining).toISOString().substr(11, 8);
+        $("#timer").html("<p> Time Remaining: " + formattedTime + " seconds</p>");
+        if (timeRemaining <= 0) {
+            document.getElementById("main").innerHTML = " Your Score Is: " + userScore + "/" + questions.length;
+            clearInterval(interval);
+        }
+        
     }
 
-    setInterval(timeInput, 100);
+    interval = setInterval(timeInput, 100);
 
-	const display = () => {
-		let html = `
+    const display = () => {
+        let answersGenerated = "";
+        for (let i = 0; i < answers[currentQuestion].length; i++) {
+            answersGenerated = answersGenerated +
+                `
+                <div class="form-check">
+                    <input class="form-check-input" type="radio" name="anyname"  value="" id="answerHtml${i}">
+                    <label class="form-check-label" for="answerHtml${i}">
+                        ${answers[currentQuestion][i]}
+                    </label>
+                </div>
+            `;
+        }
+
+        let questionHtml = `
 		<div class="card">
-                <div class="card-header">
-                    Question #${currentQuestion + 1}
-                </div>
-                <div class="card-body">
-                    <h5 class="card-title text-center">What is the answer to this question?</h5>
-                    <div class="form-check">
-                        <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
-                        <label class="form-check-label" for="flexCheckDefault">
-                            Answer 1
-                        </label>
-                    </div>
-                    <div class="form-check">
-                        <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked">
-                        <label class="form-check-label" for="flexCheckChecked">
-                            Answer 2
-                        </label>
-                    </div>
-                    <div class="form-check">
-                        <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked">
-                        <label class="form-check-label" for="flexCheckChecked">
-                            Answer 3
-                        </label>
-                    </div>
-                    <div class="form-check">
-                        <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked">
-                        <label class="form-check-label" for="flexCheckChecked">
-                            Answer 4
-                        </label>
-                    </div>
-                </div>
-            </div>`;
-	}
+            <div class="card-header">
+                Question #${currentQuestion + 1}
+            </div>
+            <div class="card-body">
+                <h5 class="card-title text-center">${questions[currentQuestion]}</h5>
+                ${answersGenerated}
+            </div>
+            <div class="card-body">
+                <button type="button" class="btn btn-success" id="submitButton">Submit!</button>
+            </div>
+        </div>`;
 
 
+        document.getElementById("main").innerHTML = questionHtml;
+        document.getElementById("submitButton").onclick = () => {
+            if (document.getElementById("answerHtml" + correctAnswers[currentQuestion]).checked) {
+                userScore = userScore + 1;
+            } else { // they got the question wrong
+                testTime -= 1000 * 60; // lose 1 minute
+            }
+            currentQuestion = currentQuestion + 1;
+            if (currentQuestion < questions.length) {
+                display();
+            } else {
+                document.getElementById("main").innerHTML = " Your Score Is: " + userScore + "/" + questions.length;
+                clearInterval(interval);
+            }
+        };
+    }
+    //When the html loads for the first time
+    display();
 });
-/*
-function funcName() {
-
-}
-
-OR
-
-const funcName = () => {
-
-}
-*/
-
-/*  HOMEWORKKKKKK ---->
-	// Vanilla JS way to edit the DOM:
-	document.getElementById("idGoesHere").innerHTML = "html goes here";
-	// Jquery way to do EXACT same thing:
-	$("#idGoesHere").html("html goes here");
-
-*/
